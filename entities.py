9cde4e0c26c70aea33bb24a9689d3f93e650d6c7
unknown <nguyenthangloi19052004@gmail.com>
2024-09-01 23:12:13 +0700

    entities & 90% handle room event

---
+++ entities.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+class User():
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.current_room = None
+    
+    def setName(self, name):
+        self.name = name
+
+    def getName(self, name):
+        self.name = name
+    
+    def set_requested_game_room(self, room):
+        self.requestedGameRoom = room
+
+    def set_game_mark(self, gameMark):
+        self.gameMark = gameMark
+
+    def start_game_intention(self, gameStart = True):
+        self.gameStartIntention = gameStart
+
+    def get_game_intention(self):
+       return self.gameStartIntention
+
+class Room():
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.lead = None
+        self.guest = None
+        self.ready_player = 0
+        self.game_play = []
+
+    def isAvailable(self):
+        if len(self.players) < 2 and self.ready_player == 2:
+            return True
+        else:
+            return False
+
+class Game():
+    def __init__(self, id):
+        self.id = id
+        self.winner = None
+        self.chessboard = np.zeros((15, 15))
+    
+    def add_player(self, objPlayer):
+        self.onlineClients.append(objPlayer)
+    
+    # def get_players_nbr(self):
+    #     return len(self.onlineClients)
+
+    # def check_players_game_start(self):
+    #     for player in self.onlineClients:
+    #         if player.get_game_intention() == False:
+    #             self.gameRound = False
+    #             return
+    #         self.gameRound = True
+
+    # def get_rand_active_player(self):
+    #     self.activePlayer = randint(0, 1)
+    #     return self.activePlayer
+
+    # def get_swap_player(self):
+    #     self.activePlayer =  int(not(bool(self.activePlayer)))
+    #     return self.activePlayer
+
+    # def get_ready_for_game(self):
+    #     self.check_players_game_start()
+    #     return self.gameRound
+
+    # def roomAvailable(self):
+    #     """
+    #     check to have only 2 players joined
+    #     """
+    #     if len(self.onlineClients) < 2:
+    #         return True
+    #     else:
+    #         return False
+
+    # def getPlayerIdx(self, sid):
+    #     """
+    #     return the player index from active game room
+    #     Arguments:
+    #     sid: id 
+    #     """
+    #     idx = 0
+    #     for player in self.onlineClients:
+    #         if player.id == sid:
+    #             return idx
+    #         idx +=1
+    
+    # def getClientsInRoom(self, requestType = 'byID'):
+    #     connectedPlayers = []
+
+    #     for player in self.onlineClients:
+    #         if requestType == 'byId':
+    #             connectedPlayers.append(player.id)
+    #         elif requestType == 'byName':
+    #             connectedPlayers.append(player.name)
+    #     return connectedPlayers
+
+    # def startRound(self):
+    #     for player in self.onlineClients:
+    #         player.gameStartIntention = False
--- server.py
+++ server.py
@@ -1,66 +1,220 @@
-from flask import Flask
+import numpy as np
+from entities import *
+from flask_cors import CORS
+from datetime import datetime
+import uuid, json, random, string
+from flask import Flask, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret!"
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app, resources = {r"/*":{
+    "origins": "*"
+}})
+
+socketio = SocketIO(app,cors_allowed_origins="*")
+
+def name_generation(length):
+    characters = string.ascii_letters
+    random_string = ''.join(random.choices(characters, k=length))
+    return random_string
+
+def serialization(data):
+    return json.dumps(data.__dict__)
+
+rooms = {}
+users = {}
+games = {}
+records = {}
+# request.ids = uuid
+uuid_mapping = {}
+
+# user event
+
+@socketio.event
+def connect():
+    user = User(id = request.sid, name = name_generation(5))
+    global users
+    users[user.id] = user
+    print(">> client {} connected to server".format(user.id))
+
+@socketio.event
+def disconnect():
+    global users
+    user = users.get(request.sid)
+    del users[request.sid]
+    print(">> client {} disconnected to server".format(user.id))
+
+@socketio.on('player_information')
+def handle_player_information(payload):
+    user = users.get(request.sid)
+    user.name = payload['name']
+    users[request.sid] = user
+    socketio.emit('player_information', {
+        "code": 200,
+        "message": 'Player information',
+        "player": serialization(user)
+    }, to = user.id)
+
+# room event
+
+@socketio.on('room_list')
+def handle_fetch_rooms(payload):
+    res = []
+    for room in rooms.values():
+        res.append(serialization(room))
+
+    socketio.emit('room_list', {
+        "code": 200,
+        "message": 'Room list',
+        "rooms": res
+    }, to = request.sid)
+
+@socketio.on('ready')
+def handle_ready(payload):
+    global rooms
+    room = rooms.get(payload['room_id'])
+
+    room.ready_player = room.ready_player + 1
+    rooms[room.id] = room
+
+    socketio.emit('room_list', {
+        "code": 200,
+        "message": 'Room list',
+        "rooms": room
+    }, to = [room.guest, room.lead])
+
+@socketio.on('wait')
+def handle_ready(payload):
+    global rooms
+    room = rooms.get(payload['room_id'])
+
+    if room.ready_player < 1:
+        
+    room.ready_player = room.ready_player - 1
+    rooms[room.id] = room
+
+    socketio.emit('room_list', {
+        "code": 200,
+        "message": 'Room list',
+        "rooms": room
+    }, to = [room.guest, room.lead])
+
+@socketio.on('ready')
+def handle_ready(payload):
+    global rooms
+    room = rooms.get(payload['room_id'])
+
+    room.ready_player = room.ready_player + 1
+    rooms[room.id] = room
+
+    socketio.emit('room_list', {
+        "code": 200,
+        "message": 'Room list',
+        "rooms": room
+    }, to = [room.guest, room.lead])
+
+@socketio.on('create_room')
+def handle_create_room(payload):
+    # create
+    room = Room(uuid.uuid4(), payload['room_name'])
+
+    # construct relationship
+    user = users.get(request.sid)
+    room.lead = users.get(request.sid)
+    user.current_room = room
+
+    # save
+    global rooms
+    rooms[room.id] = room
+
+    # response
+    socketio.emit('create_room', {
+        "code": 200,
+        "message": 'Room created',
+        "room": serialization(room)
+    }, to = request.sid)
+
+@socketio.on('join_room')
+def handle_join_room(payload):
+    global rooms
+
+    # find
+    room = rooms.get(payload['room_id'])
+
+    if room is None:
+        # if not exist
+        # response
+        socketio.emit('join_room', {
+            "code": 404,
+            "message": 'Not found room'
+        })
+    else:
+        # if exist
+        # construct relation ship
+        user = users.get(request.sid)
+        room.guest = user
+        user.current_room = room
+
+        # save
+        rooms[room.id] = room
+
+        # response
+        socketio.emit('join_room', {
+            "code": 200,
+            "message": 'Joined room',
+            "room": serialization(room)
+        }, to = [room.id, request.sid])
+
+@socketio.on('room_start')
+def handle_room_start(payload):
+    room = rooms.get(payload['room_id'])
+    if room is None or room.isAvailable() == False:
+        socketio.emit('room_start', {
+            "code": 404,
+            "message": 'Room not available or not found'
+        })
+    else:
+        game = 
+
+# @socketio.on("message")
+# def handle_message(data):
+#     print("Message received: " + str(data))
+#     socketio.emit("message", {"data": "Message received"})
+#     send("Message received hahah")
 
 
-@socketio.on("connect")
-def handle_connect():
-    print("Client connected")
-    socketio.emit("message", {"data": "Connected"})
+# @socketio.on("join")
+# def handle_join(data):
+#     room = data["room"]
+#     join_room(room)
+#     send("Joined room: " + room, room=room)
+#     print("Joined room: " + room)
 
+# @socketio.on("leave")
+# def handle_leave(data):
+#     room = data["room"]
+#     msg = data["msg"]
+#     leave_room(room)
+#     send("Left room: " + room, room=room)
+#     send(msg)
+#     print("Left room: " + room)
 
-@socketio.on("disconnect")
-def handle_disconnect():
-    print("Client disconnected")
-    send("Client disconnected hahah")
-    socketio.emit("message", {"data": "Disconnected"})
+# @socketio.on("join", namespace= "/chat")
+# def handle_join_chat(data):
+#     room = data["room"]
+#     join_room(room)
+#     send("Joined room: " + room, room=room)
+#     emit("message", {"data": "Joined room: " + room})
+#     print("Joined room: " + room)
 
+# @socketio.on("send_message_to_room")
+# def handle_send_message_to_room(data):
+#     room = data["room"]
+#     message = data["message"]
+#     send(message, room=room)
+#     print("Message sent to room: " + room)
 
-@socketio.on("message")
-def handle_message(data):
-    print("Message received: " + str(data))
-    socketio.emit("message", {"data": "Message received"})
-    send("Message received hahah")
-
-
-@socketio.on("join")
-def handle_join(data):
-    room = data["room"]
-    join_room(room)
-    send("Joined room: " + room, room=room)
-    print("Joined room: " + room)
-
-@socketio.on("leave")
-def handle_leave(data):
-    room = data["room"]
-    msg = data["msg"]
-    leave_room(room)
-    send("Left room: " + room, room=room)
-    send(msg)
-    print("Left room: " + room)
-
-@socketio.on("join", namespace= "/chat")
-def handle_join_chat(data):
-    room = data["room"]
-    join_room(room)
-    send("Joined room: " + room, room=room)
-    emit("message", {"data": "Joined room: " + room})
-    print("Joined room: " + room)
-
-@socketio.on("send_message_to_room")
-def handle_send_message_to_room(data):
-    room = data["room"]
-    message = data["message"]
-    send(message, room=room)
-    print("Message sent to room: " + room)
-
-
-
-
-
-if __name__ == "__main__":
+    if __name__ == "__main__":
     # make hot reloading 
-    socketio.run(app, debug=True)
+        socketio.run(app, debug=True)
